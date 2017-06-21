3193696dd843b73048acde2f09bc23e78247a5de
Elise Adams <neuman.elise@gmail.com>
2017-06-21 08:20:56 -0600

    Integrated the draft and published toggle method.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'gallery', to: 'pages#gallery'
 
-  resources :blogs
+  resources :blogs do
+      member do 
+        get  :toggle_status
+      end
+  end
   
   root to: 'pages#home'
 end
