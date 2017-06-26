fa7515b0726748bb28d11546473788ba4fb002f6
Elise Adams <neuman.elise@gmail.com>
2017-06-26 11:47:25 -0600

    Updated the registration to show to add users and a name feature.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :registerable and :omniauthable 
-  devise :database_authenticatable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
