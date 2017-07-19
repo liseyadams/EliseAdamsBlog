969e7243d99f3ad03bb2139b234788d0dfc55f9c
Elise Adams <neuman.elise@gmail.com>
2017-07-19 14:00:29 -0600

    Implemented the blog widget for topics.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
+  before_action :set_sidebar_topics, except: [:update, :create, :destroy, :toggle_status]
   layout "blog"
   access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   
@@ -93,5 +94,8 @@ class BlogsController < ApplicationController
                                                     :body, 
                                                     :topic_id, 
                                                     :main_image)
+    end
+    def set_sidebar_topics
+      @set_sidebar_topics = Topic.with_blogs
     end
 end
